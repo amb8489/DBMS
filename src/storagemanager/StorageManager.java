360b6a53ac708e3c86edf23ffea895f5f9c52f35
Aaron Berghash <amb8489@rit.edu>
2022-02-13 12:33:05 -0500

    comparison fix

--- src/storagemanager/StorageManager.java
+++ src/storagemanager/StorageManager.java
@@ -10,6 +10,7 @@ import common.ITable;
 import common.Page;
 import common.Table;
 import pagebuffer.PageBuffer;
+
 import java.io.File;
 
 import java.lang.reflect.Array;
@@ -30,21 +31,19 @@ public class StorageManager extends AStorageManager {
     //TODO
     @Override
     public boolean clearTableData(ITable table) {
-        if(table instanceof Table workingTable){  //cool piece of code IntelliJ made for me.
+        if (table instanceof Table workingTable) {  //cool piece of code IntelliJ made for me.
             // workingTable is a "patter var" https://openjdk.java.net/jeps/394
             ArrayList<Integer> tablePages = workingTable.getPagesThatBelongToMe();
-            for(int page: tablePages){
+            for (int page : tablePages) {
                 String filename = String.format("DB\\pages\\%d", page);
                 File tableFile = new File(filename);
-                try{
-                    if(tableFile.delete()){
+                try {
+                    if (tableFile.delete()) {
                         System.out.printf("Deleted %s\n", filename);
-                    }
-                    else {
+                    } else {
                         System.out.printf("Did not delete %s\n", filename);
                     }
-                }
-                catch(Exception e){
+                } catch (Exception e) {
                     System.err.printf("Failed to delete %s\n", filename);
                     System.err.println(e);
                     return false;
@@ -99,6 +98,7 @@ public class StorageManager extends AStorageManager {
 
             Page headPage = pb.getPageFromBuffer("" + headPtr, table);
             // add all recs
+
             RECORDS.addAll(headPage.getPageRecords());
             // next page
             headPtr = headPage.getPtrToNextPage();
@@ -116,15 +116,16 @@ public class StorageManager extends AStorageManager {
         int pkidx = ((Table) table).pkIdx();
 
         // loop though all the tables pages in order
+        Page headPage = null;
         while (headPtr != -1) {
 
-            Page headPage = pb.getPageFromBuffer("" + headPtr, table);
+            headPage = pb.getPageFromBuffer("" + headPtr, table);
             // look though all record for that page
 
             int idx = 0;
 
-
             if (headPage.getPageRecords().size() == 0) {
+
                 headPage.getPageRecords().add(record);
                 headPage.wasChanged = true;
                 return true;
@@ -133,17 +134,53 @@ public class StorageManager extends AStorageManager {
             for (ArrayList<Object> row : headPage.getPageRecords()) {
 
                 //SUSS
-                if ( (record.get(pkidx).toString()).compareTo(row.get(pkidx).toString()) < 0){
-                    headPage.getPageRecords().add(idx-1,row);
-                    headPage.wasChanged = true;
+                int pkid = ((Table) table).pkIdx();
+                String pk_type = table.getAttributes().get(pkid).getAttributeType();
 
-                    return true;
+                switch (pk_type.charAt(0)) {
+                    case 'I':
+                        if (((Integer)record.get(pkidx)).compareTo((Integer)row.get(pkidx)) < 0) {
+                            headPage.getPageRecords().add(idx, record);
+                            headPage.wasChanged = true;
+                            return true;
+                        }
+                        break;
+                    case 'D':
+                        if (((Double)record.get(pkidx)).compareTo((Double)row.get(pkidx)) < 0) {
+                            headPage.getPageRecords().add(idx, record);
+                            headPage.wasChanged = true;
+                            return true;
+                        }
+                        break;
+
+                    case 'B':
+                        if (((Boolean)record.get(pkidx)).compareTo((Boolean)row.get(pkidx)) < 0) {
+                            headPage.getPageRecords().add(idx, record);
+                            headPage.wasChanged = true;
+                            return true;
+                        }
+                        break;
+
+                    default:
+                        if ((record.get(pkidx).toString()).compareTo(row.get(pkidx).toString()) < 0) {
+                            headPage.getPageRecords().add(idx, record);
+                            headPage.wasChanged = true;
+                            return true;
+                        }
+                        break;
                 }
+
+
                 idx++;
             }
             // next page
             headPtr = headPage.getPtrToNextPage();
         }
+        // add to last spot in page in table
+        headPage.getPageRecords().add(record);
+        headPage.wasChanged = true;
+
+
         return false;
     }
 
@@ -184,6 +221,7 @@ public class StorageManager extends AStorageManager {
      * • the  primary  key  changes
      * • the  size  increases  causing  a  page  split.  This  can  also  cause  other  records  to  move  as
      * well.
+     *
      * @param table     the table to update the record in
      * @param oldRecord the old record data
      * @param newRecord the new record data
@@ -193,13 +231,13 @@ public class StorageManager extends AStorageManager {
     public boolean updateRecord(ITable table, ArrayList<Object> oldRecord, ArrayList<Object> newRecord) {
 
         // page name for head is always at idx zero
-        int headPtr = ((Table)table).getPagesThatBelongToMe().get(0);
+        int headPtr = ((Table) table).getPagesThatBelongToMe().get(0);
 
         // where in a row the pk is
         int pkidx = ((Table) table).pkIdx();
 
         // loop though all the tables pages in order
-        while(headPtr != -1) {
+        while (headPtr != -1) {
 
             Page headPage = pb.getPageFromBuffer("" + headPtr, table);
             // look through all record for that page
@@ -207,13 +245,12 @@ public class StorageManager extends AStorageManager {
                 if (oldRecord.get(pkidx) == currRec.get(pkidx)) {
                     headPage.getPageRecords().remove(oldRecord);
                     headPage.wasChanged = true;
-                    return insertRecord(table,newRecord);
+                    return insertRecord(table, newRecord);
                 }
             }
             // next page
             headPtr = headPage.getPtrToNextPage();
         }
-
 
 
         return false;
--- src/testers/FullTest.java
+++ src/testers/FullTest.java
@@ -3,24 +3,84 @@ package testers;
 import catalog.ACatalog;
 import catalog.Catalog;
 import common.Attribute;
+import common.ITable;
 import common.Page;
 import common.Table;
 import pagebuffer.PageBuffer;
 import storagemanager.AStorageManager;
 import storagemanager.StorageManager;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.SortedMap;
 
 public class FullTest {
 
+    // stole this for m the given tester to help make random strings
+    private static String getSaltString(int length) {
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < length) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        return salt.toString();
+
+    }
+    public static int tot = 0;
+    // makes a random record with the schema  " Integer Double Boolean Char(5) varchar(10)"
+    public static ArrayList<Object> mkRandomRec(Table table) {
+        tot++;
+
+
+        int charlen = 0;
+        int vacharlen = 0;
+        ArrayList<String> schema = new ArrayList<>();
+
+        // looping though table attribs to get their types
+        for (Attribute att : table.getAttributes()) {
+            schema.add(att.getAttributeType());
+
+            // found a char(#) paring for the number
+            if (att.getAttributeType().startsWith("Char(")) {
+                charlen = Integer.parseInt(att.getAttributeType().substring(5, att.getAttributeType().length() - 1));
+            }
+            if (att.getAttributeType().startsWith("Varchar(")) {
+                vacharlen = Integer.parseInt(att.getAttributeType().substring(8, att.getAttributeType().length() - 1));
+            }
+        }
+        Random r = new Random();
+        ArrayList<Object> rec = new ArrayList<>();
+        for (int idx = 0; idx < table.getAttributes().size(); idx++) {
+                // read in what the schema says is next
+                switch (schema.get(idx)) {
+                    case "Integer":
+                        rec.add(tot);
+                        break;
+                    case "Double":
+                        rec.add(r.nextDouble());
+                        break;
+                    case "Boolean":
+                        rec.add(r.nextBoolean());
+                        break;
+                    default:
+                        if (schema.get(idx).startsWith("Char(")) {
+                            rec.add(getSaltString(charlen));
+                        } else {
+                            rec.add(getSaltString(Math.abs(r.nextInt()) % vacharlen + 1));
+                        }
+                }
+        }
+
+        return rec;
+    }
+
 
     public static void main(String[] args) {
         // mk catalog
-        ACatalog cat = Catalog.createCatalog("src/DB",3048,2);
-
-        // make pb this should be made by sm ?
-        PageBuffer pb = new PageBuffer(2);
+        ACatalog cat = Catalog.createCatalog("src/DB", 1024, 2);
 
         AStorageManager sm = AStorageManager.createStorageManager();
 
@@ -32,7 +92,7 @@ public class FullTest {
         attrs.add(new Attribute("attr4", "Char(5)"));
         attrs.add(new Attribute("attr5", "Varchar(10)"));
         Attribute pk = attrs.get(0);
-        Table tab1 = (Table) cat.addTable("table1",attrs,pk);
+        Table tab1 = (Table) cat.addTable("table1", attrs, pk);
 
 
         //see init page made for this table
@@ -41,49 +101,22 @@ public class FullTest {
         System.out.println(tab1.getPagesThatBelongToMe());
 
         // insert into table
-
-        //1) if table has no pages we neeed to make its first page :)
-        Page p = pb.getPageFromBuffer("1", tab1);
-
-        // add not in any sorted order a new row given the schemea
-//        for(int i = 0; i < 30; i++) {
-//            p.getPageRecords().add(TEST_read_and_write.mkRandomRec());
-//        }
-        for(int i = 0; i < 30; i++) {
-            ArrayList<Object> row = TEST_read_and_write.mkRandomRec();
-            row.set(0, null);
-            row.set(4, null);
-            p.getPageRecords().add(row);
+        for (int i = 0; i < 30; i++) {
+            ArrayList<Object> rec = mkRandomRec(tab1);
+            sm.insertRecord(tab1,rec);
         }
-        p.wasChanged = true;
-//
-//
 
-//
-            for(ArrayList<Object> r: p.getPageRecords()){
-                    System.out.println(r);
+        sm.purgePageBuffer();
 
-            }
-
-
-        pb.PurgeBuffer();
-
-        p = pb.getPageFromBuffer("1",tab1);
-
-        System.out.println(p.calcSizeOfRecords(p.getPageRecords(),tab1));
-
+        for (ArrayList<Object> r : sm.getRecords(tab1)) {
+            System.out.println(r);
+        }
 
 
 
 
         // save
 //        cat.saveToDisk();
-
-
-
-
-
-
 
 
     }
