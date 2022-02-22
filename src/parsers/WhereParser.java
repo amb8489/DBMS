76ccec7f37cb8e847388b473b9d05abe95c9ce62
Aaron Berghash <amb8489@rit.edu>
2022-02-22 18:23:29 -0500

    fixes to table and where and create table

--- src/parsers/WhereParser.java
+++ src/parsers/WhereParser.java
@@ -3,16 +3,16 @@ package parsers;
 
 import common.Attribute;
 
-import java.lang.reflect.Type;
 import java.util.*;
 import java.util.regex.Pattern;
-import java.util.stream.IntStream;
 
 import static java.util.Map.entry;
 
 public class WhereParser {
 
     private Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+
+    private static HashMap<String, Tuple<List<String>, ArrayList<Tuple<Integer,Integer>>>> CacheWhereStmtPlacementPattern = new HashMap<>();
 
     private static final Map<String, Integer> precedence = Map.ofEntries(
             entry("and", 1),
@@ -24,10 +24,12 @@ public class WhereParser {
             entry("<=", 3),
             entry(">=", 3));
 
-    private static final Set<String> operators = precedence.keySet();
+    private static final Set operators = precedence.keySet();
 
 
     private static boolean Validate(List<String> tokens, List<Object> row) {
+
+
 
         try {
             // making stacks for shunting yard algo
@@ -61,7 +63,7 @@ public class WhereParser {
                                 List<Object> nd = Output.subList(Output.size() - 3, Output.size());
                                 Output = Output.subList(0, Output.size() - 3);
                                 Output.add(eval(nd));
-                                System.out.println("  " + Output.get(Output.size() - 1));
+//                                System.out.println("  " + Output.get(Output.size() - 1));
 
                             }
 
@@ -84,8 +86,10 @@ public class WhereParser {
                         if (Output.size() >= 3 && operators.contains(t)) {
                             List<Object> nd = Output.subList(Output.size() - 3, Output.size());
                             Output = Output.subList(0, Output.size() - 3);
+
                             Output.add(eval(nd));
-                            System.out.println("  " + Output.get(Output.size() - 1));
+
+//                            System.out.println("  " + Output.get(Output.size() - 1));
 
                         }
                     }
@@ -103,12 +107,16 @@ public class WhereParser {
                 if (Output.size() >= 3 && operators.contains(t)) {
                     List<Object> nd = Output.subList(Output.size() - 3, Output.size());
                     Output = Output.subList(0, Output.size() - 3);
+
                     Output.add(eval(nd));
-                    System.out.println("  " + Output.get(Output.size() - 1));
+
+
+
+//                    System.out.println("  " + Output.get(Output.size() - 1));
                 }
             }
             return (boolean) Output.get(Output.size() - 1);
-        }catch (Exception exp) {
+        } catch (Exception exp) {
             System.err.println(exp);
             return false;
         }
@@ -121,7 +129,9 @@ public class WhereParser {
     TODO refactor this junk using the types given in the attribs in fillString
      */
     private static Object eval(List<Object> nd) throws Exception {
-        System.out.print("eval: " + nd);
+
+
+//        System.out.print("eval: " + nd);
 
         String left = nd.get(0).toString();
         String right = nd.get(1).toString();
@@ -174,39 +184,78 @@ public class WhereParser {
 
      */
     private static List<String> fillString(String s, List<Object> r, ArrayList<Attribute> attrs) {
-        s = s.replace("(", "( ");
-        s = s.replace(")", " )");
-        s = s.replace("=", " = ");
-        s = s.replace("!=", " != ");
-        s = s.replace("<", " < ");
-        s = s.replace(">", " > ");
-        s = s.replace("<=", " <= ");
-        s = s.replace(">=", " >= ");
+
+        if(!CacheWhereStmtPlacementPattern.containsKey(s)) {
+
+            String stmt = s;
+
+            s = s.replace("(", " ( ");
+            s = s.replace(")", " ) ");
+            s = s.replace("=", " = ");
+            s = s.replace("!=", " != ");
+            s = s.replace("<", " < ");
+            s = s.replace(">", " > ");
+            s = s.replace("<=", " <= ");
+            s = s.replace(">=", " >= ");
 
 
-        List<String> tokens = new ArrayList<>(List.of(s.split(" ")));
-        System.out.println(tokens);
 
-        tokens.removeIf(String::isBlank);
-        System.out.println(tokens);
-        int idx = 0;
-        for (Attribute a : attrs) {
-//            int i = tokens.indexOf(a.attributeName());
-            int[] WhereColEqName = IntStream.range(0, tokens.size()).filter(i -> tokens.get(i).equals(a.attributeName())).toArray();
 
-            System.out.println(Arrays.toString(WhereColEqName));
+            List<String> tokens = new ArrayList<>(List.of(s.split(" ")));
+            tokens.removeIf(String::isBlank);
 
-            for(Integer i :WhereColEqName) {
-                String val = r.get(idx).toString();
-                tokens.set(i, val);
+            int whereIdx = 1;
+            for (String t : tokens) {
+                if (t.equalsIgnoreCase("where")){
+                    tokens = tokens.subList(whereIdx,tokens.size());
+                    break;
+                }
+                whereIdx++;
             }
-            idx += 1;
+
+            System.out.println(tokens);
+
+
+
+
+            HashMap<String, Integer> AttribNames = new HashMap<>();
+            for (int i = 0; i < attrs.size(); i++) {
+                AttribNames.put(attrs.get(i).getAttributeName(), i);
+            }
+
+
+            // loop though tokens
+
+            int tokenIdx = 0;
+            ArrayList<Tuple<Integer, Integer>> IdxsToReplace = new ArrayList<>();
+            for (String t : tokens) {
+                // if that token is a column name in the table, (aka an attribute name)
+                if (AttribNames.containsKey(t)) {
+                    // set that token to the value from the given row at the idx of the col name
+                    Integer attribIdx = AttribNames.get(t);
+
+                    IdxsToReplace.add(new Tuple<>(tokenIdx,attribIdx));
+
+                    tokens.set(tokenIdx, r.get(attribIdx).toString());
+                }
+                tokenIdx++;
+            }
+            Tuple<List<String>,ArrayList<Tuple<Integer,Integer>>> tup = new Tuple<>(tokens,IdxsToReplace);
+            CacheWhereStmtPlacementPattern.put(stmt,tup);
+            return tokens;
         }
+
+
+        Tuple<List<String>, ArrayList<Tuple<Integer, Integer>>> cached = CacheWhereStmtPlacementPattern.get(s);
+        List<String>tokens = cached.x;
+        ArrayList<Tuple<Integer, Integer>>replaceAt = cached.y;
+
+        for(Tuple<Integer,Integer> t:replaceAt){
+            tokens.set(t.x, r.get(t.y).toString() );
+        }
+
         return tokens;
     }
-
-
-
 
     /*--------------    HOW TO USE  -------------------
 
@@ -222,8 +271,7 @@ public class WhereParser {
     if you want to delete:
     "delete from foo where (Fname = Aaron and Gpa < 3) or HeightI = 71"
 
-    1) you will pass in ONLY what comes after where and removing the semi-colon at the end:
-       "(Fname = Aaron and Gpa < 3) or HeightI = 71"
+    1) you will pass in "delete from foo where (Fname = Aaron and Gpa < 3) or HeightI = 71"
 
     2) the row values (aaron,berg,3.4,71,23)
 
@@ -240,15 +288,32 @@ public class WhereParser {
        - ASK about "" for strings
        - possible refactor of this entire thing to better accommodate eval function
      */
-    public static boolean whereIsTrue(String stmt, List<Object> row, ArrayList<Attribute> attrs) {
-        List<String> tokens = fillString(stmt, row, attrs);
-        return Validate(tokens, row);
-    }
+    public boolean whereIsTrue(String stmt, List<Object> row, ArrayList<Attribute> attrs) {
 
+        long startTime = System.currentTimeMillis();
+        List<String> tokens = fillString(stmt, row, attrs);
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+
+
+        startTime = System.currentTimeMillis();
+        boolean res =  Validate(tokens, row);
+        endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime);
+        System.out.println("-----------------------------");
+
+        return res;
+    }
 
 
     /////////////////////////////////////////// EXAMPLE
     public static void main(String[] args) {
+
+
+        WhereParser parser = new WhereParser();
+
+
+
 
         // TALE ATTRIBUTES
         ArrayList<Attribute> attrs = new ArrayList<>();
@@ -269,12 +334,41 @@ public class WhereParser {
         r.add(1);
 
         // STMT
+        long startTime = System.currentTimeMillis();
 
-        String s = "(fName=\"AArON\" or gpa>2.0) and (lName=berg or 2 < 2) or gpa > 1";
-        System.out.println(s);
-        System.out.println("STMT IS :"+whereIsTrue(s, r, attrs));
+        for (int i = 0; i < 1; i++) {
+            String s = "delete from foo where (fName=\"AArON\" or gpa>2.0) and (lName=berg or 2<2) or gpa>1";
+
+            boolean res = parser.whereIsTrue(s, r, attrs);
+//            System.out.println("STMT IS :" + res);
+            r.set(3,i);
+        }
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println(endTime - startTime);
 
     }
 
+    /**
+     *
+     * @param <X> element of type X
+     * @param <Y> element of type Y
+     */
+    public static class Tuple<X,Y>{
+        public X x;  //element 1
+        public Y y;  //element 2
 
+        public Tuple(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return x + " "+y;
+        }
+    }
 }
+
+
